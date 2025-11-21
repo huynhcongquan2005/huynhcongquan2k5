0a96085dd8a30472230944d59b799ad9fb1ea291
huynhcongquan2005 <huynhcongquan3963@gmail.com>
2025-11-21 13:30:53 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Huỳnh Công Quân đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
